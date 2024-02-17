0f1a1145653161e115ed855d9125869d4a7a8ec9
MANAV AHUJA <ahujamanav43@gmail.com>
2024-02-17 21:24:22 +0530

    ans

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,8 +1,20 @@
 package com.driver;
 
 public class Boat implements WaterVehicle {
+
     String name;
     int capacity;
+    public Boat(String name,int capacity){
+        this.name=name;
+        this.capacity=capacity;
+
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setCapacity(int capacity){
+        this.capacity=capacity;
+    }
 
     @Override
     public int getVehicleCapacity() {
